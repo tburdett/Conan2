cf363acf8f2abb8eaceb93d7a07a587e998f0dd5
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-01-11 15:45:53 +0000

    Added ENA SRA processing code.
    git-svn-id: svn://bar.ebi.ac.uk/trunk/fgpt/automation/conan2@18850 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ conan-biosd-processes/src/main/java/uk/ac/ebi/fgpt/conan/process/biosd/ENASRAWebDownloadLSFProcess.java
@@ -0,0 +1,60 @@
+package uk.ac.ebi.fgpt.conan.process.biosd;
+
+import net.sourceforge.fluxion.spi.ServiceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.fgpt.conan.lsf.AbstractLSFProcess;
+import uk.ac.ebi.fgpt.conan.lsf.LSFProcess;
+import uk.ac.ebi.fgpt.conan.model.ConanParameter;
+import uk.ac.ebi.fgpt.conan.properties.ConanProperties;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@ServiceProvider
+public class ENASRAWebDownloadLSFProcess extends AbstractBioSDLSFProcess {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	public String getName() {
+		return "updatesourceenasra";
+	}
+
+	protected Logger getLog() {
+		return log;
+	}
+	
+	protected String getCommand(Map<ConanParameter, String> parameters)
+			throws IllegalArgumentException {
+		getLog().debug(
+				"Executing " + getName() + " with the following parameters: "
+						+ parameters.toString());
+
+		// deal with parameters
+		SampleTabAccessionParameter accession = new SampleTabAccessionParameter();
+		accession.setAccession(parameters.get(accessionParameter));
+		if (accession.getAccession() == null) {
+			throw new IllegalArgumentException("Accession cannot be null");
+		}
+
+		String scriptpath = ConanProperties
+				.getProperty("biosamples.script.path");
+		File script = new File(scriptpath, "ENASRAWebDownload.sh");
+		File outdir;
+		try {
+			outdir = getOutputDirectory(accession);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to create directories for "+accession.getAccession());
+		}
+		
+		// main command to execute script
+		String mainCommand = script.getAbsolutePath() + " "
+				+ accession.getAccession() + " " + outdir.getAbsolutePath();
+		getLog().debug("Command is: <" + mainCommand + ">");
+		return mainCommand;
+	}
+}
