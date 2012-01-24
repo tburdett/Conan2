76aa0e1fda461050aa7e586e823ef8a63ccb74c2
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-01-24 11:46:14 +0000

    in development version of MageTabToSampleTab Daemon
    git-svn-id: svn://bar.ebi.ac.uk/trunk/fgpt/automation/conan2@18954 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ conan-biosd-processes/src/main/java/uk/ac/ebi/fgpt/conan/process/biosd/dao/MageTabDaemonInputs.java
@@ -0,0 +1,43 @@
+package uk.ac.ebi.fgpt.conan.process.biosd.dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.ebi.fgpt.conan.dao.ConanDaemonInputsDAO;
+import uk.ac.ebi.fgpt.conan.model.ConanParameter;
+import uk.ac.ebi.fgpt.conan.process.biosd.model.SampleTabAEAccessionParameter;
+import uk.ac.ebi.fgpt.conan.properties.ConanProperties;
+
+public class MageTabDaemonInputs implements ConanDaemonInputsDAO {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+    
+    public Class<? extends ConanParameter> getParameterType() {
+        return SampleTabAEAccessionParameter.class;
+    }
+
+    
+    public List<String> getParameterValues() {
+        //this must get a list of NEW parameter values to run
+        //in this context this means ones that have not been copied yet
+        
+
+        String sampletabpath = ConanProperties
+                .getProperty("biosamples.sampletab.path");
+        
+        List<String> parametervalues = new ArrayList<String>();
+        
+        //TODO finish
+        
+        return parametervalues;
+    }
+
+}
