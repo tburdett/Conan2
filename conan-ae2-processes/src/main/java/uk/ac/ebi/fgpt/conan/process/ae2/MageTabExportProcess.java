1c533870ec623289ab2afffc609361eebca5258e
tobias <tobias@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-06-27 09:07:57 +0000

    Added MAGE-TAB Export process.
    git-svn-id: svn://bar.ebi.ac.uk/trunk/fgpt/automation/conan2@20168 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ conan-ae2-processes/src/main/java/uk/ac/ebi/fgpt/conan/process/ae2/MageTabExportProcess.java
@@ -0,0 +1,93 @@
+package uk.ac.ebi.fgpt.conan.process.ae2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.fgpt.conan.ae.AccessionParameter;
+import uk.ac.ebi.fgpt.conan.lsf.LSFProcess;
+import uk.ac.ebi.fgpt.conan.model.ConanParameter;
+import uk.ac.ebi.fgpt.conan.properties.ConanProperties;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Process for MAGE-TAB exporting.
+ *
+ * @author Tobias Ternent
+ * @date 20-Jun-2012
+ */
+public class MageTabExportProcess extends AbstractAE2LSFProcess {
+    private final Collection<ConanParameter> parameters;
+    private final AccessionParameter accessionParameter;
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    public MageTabExportProcess() {
+        parameters = new ArrayList<ConanParameter>();
+        accessionParameter = new AccessionParameter();
+        parameters.add(accessionParameter);
+    }
+
+    protected Logger getLog() {
+        return log;
+    }
+
+    public String getName() {
+        return "MAGE-TAB Export";
+    }
+
+    public Collection<ConanParameter> getParameters() {
+        return parameters;
+    }
+
+    protected String getComponentName() {
+        return LSFProcess.UNSPECIFIED_COMPONENT_NAME;
+    }
+
+    protected String getCommand(Map<ConanParameter, String> parameters) throws IllegalArgumentException {
+        getLog().debug("Executing " + getName() + " with the following parameters: " + parameters.toString());
+
+        // deal with parameters
+        AccessionParameter accession = new AccessionParameter();
+        accession.setAccession(parameters.get(accessionParameter));
+        if (accession.getAccession() == null) {
+            throw new IllegalArgumentException("Accession cannot be null");
+        }
+        else {
+            String environmentPath = ConanProperties.getProperty("environment.path");
+            String publicFTPPath = ConanProperties.getProperty("ftp.path")  +
+                                    "/MAGE-TAB-EXPORT/" + accession.getSubDir();;
+            return environmentPath  + "software/magetab_exporter_0.0.1-SNAPSHOT/magetab_exporter.sh "
+                + accession.getAccession()
+                + " --no-adf "
+                + " --output " + publicFTPPath
+                ;
+        }
+    }
+
+    protected String getLSFOutputFilePath(Map<ConanParameter, String> parameters)
+            throws IllegalArgumentException {
+        getLog().debug("Executing " + getName() + " with the following parameters: " + parameters.toString());
+
+        // deal with parameters
+        AccessionParameter accession = new AccessionParameter();
+        accession.setAccession(parameters.get(accessionParameter));
+        if (accession.getAccession() == null) {
+            throw new IllegalArgumentException("Accession cannot be null");
+        }
+        else {
+            // get the mageFile parent directory
+            final File parentDir = accession.getFile().getAbsoluteFile().getParentFile();
+
+            // files to write output to
+            final File outputDir = new File(parentDir, ".conan");
+
+            // lsf output file
+            return new File(outputDir, "idfexport.lsfoutput.txt").getAbsolutePath();
+        }
+    }
+}
+
+
