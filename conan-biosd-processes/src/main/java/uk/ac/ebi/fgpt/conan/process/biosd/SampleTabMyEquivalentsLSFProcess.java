0ff5bf910729138d71cb27fc027dbd21f1785c04
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-01-07 12:36:53 +0000

    Fix command spacing
    git-svn-id: svn://bar.ebi.ac.uk/trunk/fgpt/automation/conan2@22055 2913f559-6b04-0410-9a09-c530ee9f5186

--- conan-biosd-processes/src/main/java/uk/ac/ebi/fgpt/conan/process/biosd/SampleTabMyEquivalentsLSFProcess.java
+++ conan-biosd-processes/src/main/java/uk/ac/ebi/fgpt/conan/process/biosd/SampleTabMyEquivalentsLSFProcess.java
@@ -67,7 +67,7 @@ public class SampleTabMyEquivalentsLSFProcess extends AbstractBioSDLSFProcess {
         
 		// main command to execute script
 		String mainCommand = script.getAbsolutePath() 
-				+ sampletabFile.getAbsolutePath()
+				+ " "+sampletabFile.getAbsolutePath()
                 + " &> "+logfile.getAbsolutePath();
 		getLog().info("Command is: <" + mainCommand + ">");
 		return mainCommand;
