87cb4a6dec99ab84435bf0efd35e27962204e8c4
emma <emma@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-06-18 13:22:05 +0000

    Removed log
    git-svn-id: svn://bar.ebi.ac.uk/trunk/fgpt/automation/conan2@24005 2913f559-6b04-0410-9a09-c530ee9f5186

--- conan-ui/src/main/java/uk/ac/ebi/fgpt/conan/web/listener/ConanEnvironmentListener.java
+++ conan-ui/src/main/java/uk/ac/ebi/fgpt/conan/web/listener/ConanEnvironmentListener.java
@@ -8,26 +8,19 @@ package uk.ac.ebi.fgpt.conan.web.listener;
  * Checks the environment at startup and sets some sensible defaults if expected values are missing.
  */
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ConanEnvironmentListener implements ServletContextListener {
 
-    private Logger log = LoggerFactory.getLogger(getClass());
-
-    protected Logger getLog() {
-        return log;
-    }
 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         String conanPath= servletContextEvent.getServletContext().getContextPath();
         System.setProperty("conan.home", conanPath);
     }
      public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        getLog().info("Shutting down Conan.");
+     //  getLog().info("Shutting down Conan.");
     }
 
 }
